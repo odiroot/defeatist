ea4759b77e1550d3c5447a2287222702b7cac68f
Michal Odnous <odi.root@gmail.com>
2014-08-24 13:05:04 +0200

    Removing and adding subreddits with empty configuration.

--- src/js/options.js
+++ src/js/options.js
@@ -31,13 +31,23 @@ ConfigTable.prototype.render = function render() {
     this.fillTable();
 };
 
+ConfigTable.prototype.update = function() {
+    this.callback(this.config);
+    this.render();
+};
+
 ConfigTable.prototype.clearTable = function clearTable() {
     this.$tbody.empty();
 };
 
 ConfigTable.prototype.fillTable = function fillTable() {
-    for(var sub in this.config) {
-        this.renderRow(sub, this.config[sub]);
+    var $row,
+        sub;
+
+    for(sub in this.config) {
+        $row = this.renderRow(sub, this.config[sub]);
+        // Attach event handlers.
+        this.listenRow($row, sub);
     }
 };
 
@@ -50,6 +60,21 @@ ConfigTable.prototype.renderRow = function renderRow(name, config) {
     $row.find('.downvote')[0].checked = config.downvote || false;
 
     this.$tbody.append($row);
+    return $row;
+};
+
+ConfigTable.prototype.listenRow = function listenRow($row, name) {
+    $row.on('click', '.remove', this.removeSub.bind(this, name));
+};
+
+ConfigTable.prototype.removeSub = function(name) {
+    delete this.config[name];
+    this.update();
+};
+
+ConfigTable.prototype.addSub = function addSub(name) {
+    this.config[name] = {};
+    this.update();
 };
 
 
@@ -59,6 +84,15 @@ function main() {
     readSubreddits(function(subs) {
         table = new ConfigTable(subs, writeSubreddits);
         table.render();
+    });
+
+    // Handling add subreddit form.
+    $('.add button').click(function() {
+        var name = $('.add input').val().trim();
+        if(!name) {
+            return;
+        }
+        table.addSub(name);
     });
 }
 
