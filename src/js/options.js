d59fbcea81b643700b66c6310e83210bab392a82
Michal Odnous <odi.root@gmail.com>
2014-08-24 12:32:04 +0200

    Reading subreddit config into table.

--- src/js/options.js
+++ src/js/options.js
@@ -1,7 +1,54 @@
 /* global $ */
 'use strict';
+var STORAGE = chrome.storage.sync;
 
 
-$(function() {
-    console.log('Options code running.');
-});
+function readSubreddits(callback) {
+    STORAGE.get('subreddits', function(items) {
+        callback(items.subreddits);
+    });
+}
+
+
+function writeSubreddits(subreddits) {
+    STORAGE.set({
+        'subreddits': subreddits
+    });
+}
+
+
+function renderRow(name, config) {
+    var template = $('#tableRow').text(),
+        $row = $(template);
+
+    // Fill rendered HTML with subreddit config.
+    $row.find('.name').text(name);
+    $row.find('.hide')[0].checked = config.hide || false;
+    $row.find('.downvote')[0].checked = config.downvote || false;
+
+    $('table tbody').append($row);
+}
+
+
+function clearTable() {
+    $('table tbody tr').remove();
+}
+
+
+function fillTable(subs) {
+    for(var sub in subs) {
+        renderRow(sub, subs[sub]);
+    }
+}
+
+
+function main() {
+    readSubreddits(function(subs) {
+        clearTable();
+        fillTable(subs);
+    });
+}
+
+
+/* Startup */
+$(main);
